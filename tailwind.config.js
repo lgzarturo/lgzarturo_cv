dd5a86d56caccc246de8f21821b5e5fcf1c409ec
Arturo Lopez <lgzarturo@gmail.com>
2025-09-21 18:52:24 -0500

    feat: se integró Tailwind CSS y estilos personalizados

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+const typography = require('@tailwindcss/typography');
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'class',
+  theme: {
+  },
+  plugins: [typography],
+};
